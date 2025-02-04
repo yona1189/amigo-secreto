a0722cf4dcfda6f8527b20cd531af09ada650664
Yonatan MG <yona20mg@hotmail.com>
2025-02-04 06:24:10 -0500

    Se crea la funcion agregar amigos al array

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//creamos un  arreglo para guardar todos los amigos que el usuario vaya ingresando en el juego
+
+//Creamos la funcion para agregar un amigo al array
+const listaAmigos= [];
+function agregarAmigo() {
+   
+    let amigo = document.getElementById('amigo').value;
+    console.log (`Su amigo secreto es: ${amigo}`);
+
+    //validando que el campo nombre del amigo sea valido
+    if (amigo == "" || amigo == null || amigo == undefined) {
+        alert("Por favor ingrese el nombre de un amigo secreto");
+    }else {
+        //agregamos un amigo al arreglo
+listaAmigos.push(amigo);
+document.getElementById('amigo').value = "";
+console.log(listaAmigos);
+    }
+}
+
